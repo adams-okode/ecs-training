3bfe91f2719b3ba596c7943a5f2a15cd7b45ef50
Anyungu <anyungucw@gmail.com>
2020-08-20 12:53:41 +0300

    todo

---
+++ todos/src/main/java/com/aws/todos/entities/todos.java
@@ -0,0 +1,34 @@
+package main.java.com.aws.todos.entities;
+
+@Entity(name = "todos")
+public class Todos {
+
+    private String id;
+    private String userId;
+    private String activityId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(String activityId) {
+        this.activityId = activityId;
+    }
+
+}
---
+++ todos/src/main/java/com/aws/todos/repos/TodosRepo.java
@@ -0,0 +1,5 @@
+package main.java.com.aws.todos.repos;
+
+public interface TodosRepo extends JpaRepository {
+
+}
