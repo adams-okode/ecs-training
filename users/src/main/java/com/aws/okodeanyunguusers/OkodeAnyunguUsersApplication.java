99f6427e09701ac1280358b7eaa5584825df55a0
Anyungu <anyungucw@gmail.com>
2020-08-21 14:10:01 +0300

    deploy users

--- users/src/main/java/com/aws/okodeanyunguusers/OkodeAnyunguUsersApplication.java
+++ users/src/main/java/com/aws/okodeanyunguusers/OkodeAnyunguUsersApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OkodeAnyunguUsersApplication {
 
-//users
+	// users users
 	public static void main(String[] args) {
 		SpringApplication.run(OkodeAnyunguUsersApplication.class, args);
 	}
