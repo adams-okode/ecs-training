b8390b61cde894e6abb66399efdf05e5ac0eac7a
adams-okode <adamsokode@gmail.com>
2020-08-24 20:20:23 +0300

    it has worked

--- consul/consul-cluster-1/init.sh
+++ consul/consul-cluster-1/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Set variables
-OUR_SERVER_ADDRESS=$(ip -4 addr show lo0 | grep -Po 'inet \K[\d.]+')
+OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
 #Collect sample json file
 SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
@@ -11,6 +11,6 @@ SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
 JSON_STRING=$(jq -n --arg snadress "$OUR_SERVER_ADDRESS" "$SAMPLE_FILE")
 
 echo $JSON_STRING >> /etc/consul.d/consul.json
-
+echo $(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 #Initialize the consul agent
 consul agent -config-file /etc/consul.d/consul.json 
