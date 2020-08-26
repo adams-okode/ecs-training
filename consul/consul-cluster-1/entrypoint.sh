391f83607acd64950375a5148c668144bcd7b665
adams-okode <adamsokode@gmail.com>
2020-08-27 00:18:52 +0300

    even more problems

--- consul/consul-cluster-1/entrypoint.sh
+++ consul/consul-cluster-1/entrypoint.sh
@@ -2,4 +2,4 @@
 
 SERVER_IP=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
-# export CONSUL_HTTP_ADDR="$SERVER_IP:8500"
+echo "CONSUL_HTTP_ADDR=$SERVER_IP:8500" >> /proc/1/environ
--- consul/consul-cluster-1/init.sh
+++ consul/consul-cluster-1/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set variables
-OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+OUR_SERVER_ADDRESS=$(ip addr show wlp2s0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
 # Collect sample json file
 SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
@@ -9,7 +9,7 @@ SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
 # Read in template one line at the time, and replace variables (more natural (and efficient) way, thanks to Jonathan Leffler).
 JSON_STRING=$(jq -n --arg snadress "$OUR_SERVER_ADDRESS" "$SAMPLE_FILE")
 
-echo $JSON_STRING >>/etc/consul.d/consul.json
+echo $JSON_STRING >> /etc/consul.d/consul.json
 
 # Initialize the consul agent
 envoy -c /etc/envoy/envoy.yaml
