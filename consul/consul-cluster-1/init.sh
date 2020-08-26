35bfcf6ddd1aaf168ca578e353f9b0f69c234eb2
adams-okode <adamsokode@gmail.com>
2020-08-26 12:14:00 +0300

    envoy proxy implementation

--- consul/consul-cluster-1/init.sh
+++ consul/consul-cluster-1/init.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-#Set variables
+# Set variables
 OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
-#Collect sample json file
+# Collect sample json file
 SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
 
-#Read in template one line at the time, and replace variables (more
-#natural (and efficient) way, thanks to Jonathan Leffler).
+# Read in template one line at the time, and replace variables (more natural (and efficient) way, thanks to Jonathan Leffler).
 JSON_STRING=$(jq -n --arg snadress "$OUR_SERVER_ADDRESS" "$SAMPLE_FILE")
 
 echo $JSON_STRING >> /etc/consul.d/consul.json
 
 echo $(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-#Initialize the consul agent
+
+# Initialize the consul agent
+envoy -c /etc/envoy/envoy.yaml
 
 consul agent -config-file /etc/consul.d/consul.json
