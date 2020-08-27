448ba1802dcd045948f53a9d01f15b46b11a2fba
Anyungu <anyungucw@gmail.com>
2020-08-27 09:55:16 +0300

    problems

--- consul/consul-cluster-1/registeractivities.sh
+++ consul/consul-cluster-1/registeractivities.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-OUR_SERVER_ADDRESS=$(ip addr show wlo1 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+OUR_SERVER_ADDRESS=$(ip addr show eht0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
 
 consul connect -http-addr "${OUR_SERVER_ADDRESS}:8500" -grpc-addr ${OUR_SERVER_ADDRESS}:8502" envoy -sidecar-for activites-1
