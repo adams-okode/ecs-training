e1578153b79722f382b694966c8eee042d365b73
adams-okode <adamsokode@gmail.com>
2020-08-28 18:03:54 +0300

    cluster 1 update

--- consul/consul-cluster-1/scripts/init.sh
+++ consul/consul-cluster-1/scripts/init.sh
@@ -4,7 +4,7 @@
 OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
 # Collect sample json file
-SAMPLE_FILE=$(cat ./opt/consul/consul.sample.json)
+SAMPLE_FILE=$(cat ./opt/consul/configs/consul.sample.json)
 
 # Read in template one line at the time, and replace variables (more natural (and efficient) way, thanks to Jonathan Leffler).
 JSON_STRING=$(jq -n --arg snadress "$OUR_SERVER_ADDRESS" "$SAMPLE_FILE")
