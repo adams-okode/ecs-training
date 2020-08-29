b54909feae57c59632732f446ef4446842007f0f
Anyungu <anyungucw@gmail.com>
2020-08-29 15:59:27 +0300

    reboot consul

--- consul/consul-cluster-1/scripts/init.sh
+++ consul/consul-cluster-1/scripts/init.sh
@@ -3,7 +3,7 @@
 # Set variables
 OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
 
-# Collect sample json file 
+# Collect sample json file reboot
 # retry users
 SAMPLE_FILE=$(cat ./opt/consul/configs/consul.sample.json)
 
